990523d81354754c162e9927c50740bf6e32a59e
Hau-Do <haudotien@gmail.com>
2018-01-25 17:10:23 +0700

    implemented Route Configuration, Navigation, Parameterised Routes, Nested Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, Component, Injectable} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {JsonpModule, Jsonp, Response} from '@angular/http';
+import {ReactiveFormsModule, FormControl, FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from "@angular/router";
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/toPromise';
 
-import { AppComponent } from './app.component';
+import { SearchService } from './app.component';
+
+import { 
+  AppComponent, 
+  HomeComponent, 
+  SearchComponent, 
+  HeaderComponent,
+  ArtistComponent,
+  ArtistTrackListComponent,
+  ArtistAlbumListComponent
+} from './app.component';
+import { from } from 'rxjs/observable/from';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'find', redirectTo: 'search'},
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent },
+  {path: 'artist/:artistId', component: ArtistComponent,
+      children: [
+        {path:'', redirectTo: 'tracks', pathMatch: 'full'},
+        {path: 'tracks', component: ArtistTrackListComponent},
+        {path: 'albums', component: ArtistAlbumListComponent}
+      ]
+  },
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    HeaderComponent,
+    ArtistComponent,
+    ArtistTrackListComponent,
+    ArtistAlbumListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    JsonpModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
